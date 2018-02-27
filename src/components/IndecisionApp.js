2560d46a59b06191bca66ff12298d4638489d791
Neil188 <19958604+Neil188@users.noreply.github.com>
2018-02-27 21:17:08 +0000

    Starter files
    create an index file to start building app around

---
+++ src/components/IndecisionApp.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+const subTitle = 'Put your life in the hands of a computer';
+
+const IndecisionApp = () => (
+    <div>
+        <h1>{subTitle}</h1> 
+        <div>
+            Under construction
+        </div>
+    </div>
+);
+
+
+export default IndecisionApp;
